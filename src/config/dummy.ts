146b637413f72b0232aac71e22573d30c921018c
Dvalo <guram.dvalishvili99@gmail.com>
2022-02-13 01:34:49 +0400

    Add temporary dummy data

---
+++ src/config/dummy.ts
@@ -0,0 +1,70 @@
+const collectionsDummy = [
+  {
+    id: 1,
+    title: "House Chores",
+    color: "Pink",
+    icon: "house",
+    tasks: [
+      {
+        id: 1,
+        title: "Do dishes",
+        completed: false,
+        created: "",
+        due: "",
+      },
+      {
+        id: 2,
+        title: "Clean up table",
+        completed: false,
+        created: "",
+        due: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "University",
+    color: "Green",
+    icon: "education",
+    tasks: [
+      {
+        id: 1,
+        title: "Study something from some book",
+        completed: true,
+        created: "",
+        due: "",
+      },
+      {
+        id: 2,
+        title: "Write essay for the class",
+        completed: false,
+        created: "",
+        due: "",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Work",
+    color: "Indigo",
+    icon: "work",
+    tasks: [
+      {
+        id: 1,
+        title: "Finish up front-end application",
+        completed: true,
+        created: "",
+        due: "",
+      },
+      {
+        id: 2,
+        title: "Fix bugs and optimize code",
+        completed: false,
+        created: "",
+        due: "",
+      },
+    ],
+  },
+];
+
+export { collectionsDummy };
