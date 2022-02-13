544fb91415472b5d4a6f966927968098f17852a5
Dvalo <guram.dvalishvili99@gmail.com>
2022-02-14 01:16:10 +0400

    Add active state to the sidebar menu item

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,27 +1,38 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import CollectionItem from "@components/collection/CollectionItem";
 import { useTasksContext } from "@context/TasksProvider";
 
 function Sidebar() {
   const { tasks } = useTasksContext();
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <aside className="fixed bg-secondary h-full w-64">
       <h1 className="font-bold text-gray-300 tracking-wide text-lg p-6">
         Collections
       </h1>
       <div className="flex flex-col">
-        {tasks && tasks.map((collection) => (
-          <Link
-            href={`/collections/${collection.id}`}
-            passHref
-            key={collection.id}
-          >
-            <a className="px-6 py-2 ease-out duration-300 mb-2 last:mb-0 hover:bg-slate-100/10">
-              <CollectionItem collection={collection} />
-            </a>
-          </Link>
-        ))}
+        {tasks &&
+          tasks.map((collection) => (
+            <Link
+              href={`/collections/${collection.id}`}
+              passHref
+              key={collection.id}
+            >
+              <a
+                className={`${
+                  collection.id === Number(id)
+                    ? "bg-slate-100/10"
+                    : "bg-transparent"
+                } px-6 py-2 ease-out duration-300 mb-2 last:mb-0 hover:bg-slate-100/10`}
+              >
+                <CollectionItem collection={collection} />
+              </a>
+            </Link>
+          ))}
       </div>
     </aside>
   );
