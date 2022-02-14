4e6831d634576603530322388b2a2c4d9c0d3ec4
Dvalo <guram.dvalishvili99@gmail.com>
2022-02-14 18:24:25 +0400

    Add close icon for custom modal component

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from "react";
+import Button from "@components/Button";
+import { ButtonVariationsEnum } from "@config/color-map";
+import { XCircleIcon } from "@heroicons/react/outline";
 
 type IProps = {
   title: string;
@@ -12,27 +15,26 @@ function Modal({ title, active, children, handleClose }: IProps) {
     <>
       <div
         className={`${
-          active ? "opacity-100 visible" : "opacity-0 invisible"
-        } fixed w-full h-full left-0 top-0 transition-all duration-300 ease-in bg-black/90`}
+          active ? "visible opacity-100" : "invisible opacity-0"
+        } fixed left-0 top-0 h-full w-full bg-black/90 transition-all duration-300 ease-in`}
         onClick={handleClose}
       />
       <div
         className={`${
-          active ? "opacity-100 visible mt-0" : "opacity-0 invisible -mt-14"
-        } pointer-events-none fixed left-0 w-full h-full top-0 transition-all duration-300 ease-in`}
+          active ? "visible mt-0 opacity-100" : "invisible -mt-14 opacity-0"
+        } pointer-events-none fixed left-0 top-0 h-full w-full transition-all duration-300 ease-in`}
       >
-        <div className="pointer-events-auto mt-28 mx-auto max-w-md w-full p-4 rounded-lg bg-secondary">
-          <div className="flex flex-col justify-between h-full">
+        <div className="pointer-events-auto mx-auto mt-28 w-full max-w-md rounded-lg bg-secondary p-4">
+          <div className="flex h-full flex-col justify-between">
             <div className="modal-content">
-              <div className="flex justify-between mb-6">
-                <h3 className="text-gray-300 font-bold mb-4">{title}</h3>
-                <button
-                  type="button"
-                  className="text-white bg-red-600 hover:bg-red-700 focus:ring-2 focus:ring-red-800/50 font-medium rounded-lg text-sm px-5 py-2 text-center"
-                  onClick={handleClose}
+              <div className="mb-6 flex items-center justify-between">
+                <h3 className="font-bold text-gray-300">{title}</h3>
+                <Button
+                  type={ButtonVariationsEnum.error}
+                  handleClick={handleClose}
                 >
-                  Close
-                </button>
+                  <XCircleIcon className="h-6 w-6" />
+                </Button>
               </div>
 
               {children}
